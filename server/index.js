4962a77734832f5591bae07ef9fe6aa22cf3f7ca
Andrey <a.so777@live.ca>
2020-10-30 16:10:57 -0700

    Implemented scraping endpoint

--- server/index.js
+++ server/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const scrape = require('website-scraper');
+const path = require('path');
+
+const PORT = 3001;
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(cors());
 
+app.get('/website', (req, res) => {
+  const { url } = req.query;
+  if (!url) {
+    res.status(400).json({
+      status: 'Bad Request',
+      data:
+        'Please provide URL of the website you want to scrape as a query parameter.',
+    });
+  }
+  const urlToConvert = new URL(url);
+  const directory = path.resolve(__dirname, `./public/${urlToConvert.hostname}`);
 
+  const options = {
+    urls: [url],
+    directory,
+  };
+
+  scrape(options).then(result => {
+    res.status(200).json({
+        status: 'success',
+        data: `127.0.0.1:${PORT}/${urlToConvert.hostname}`
+    })
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is now live at ${PORT}`);
+});
